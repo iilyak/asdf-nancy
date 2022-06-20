67ae16b0eca0f1566094d4e54fb725b656bbd200
ILYA Khlopotov <iilyak@apache.org>
2022-06-20 12:30:11 -0700

    Adjust template to support nanacy

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eu
+[ "${BASH_VERSINFO[0]}" -ge 3 ] && set -o pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for nancy.
 GH_REPO="https://github.com/sonatype-nexus-community/nancy"
 TOOL_NAME="nancy"
 TOOL_TEST="nancy --version"
@@ -14,7 +14,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if nancy is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -27,11 +26,10 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//'
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
   # Change this function if nancy has other means of determining installable versions.
   list_github_tags
 }
@@ -41,8 +39,7 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for nancy
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="$GH_REPO/releases/v${version}/nancy-v${version}-$(get_platform)-$(get_arch)"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -58,8 +55,8 @@ install_version() {
   fi
 
   (
-    mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    mkdir -p "$install_path/bin"
+    cp -r "$ASDF_DOWNLOAD_PATH/$TOOL_NAME" "$install_path/bin/$TOOL_NAME"
 
     # TODO: Asert nancy executable exists.
     local tool_cmd
@@ -69,6 +66,38 @@ install_version() {
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
     rm -rf "$install_path"
-    fail "An error ocurred while installing $TOOL_NAME $version."
+    fail "An error occurred while installing $TOOL_NAME $version."
   )
+}
+
+get_platform () {
+    local silent=${1:-}
+    local platform=""
+
+    platform="$(uname | tr '[:upper:]' '[:lower:]')"
+
+    case "$platform" in
+        linux|darwin|freebsd)
+            [ -z "$silent" ] && msg "Platform '${platform}' supported!"
+            ;;
+        *)
+            fail "Platform '${platform}' not supported!"
+            ;;
+    esac
+
+    printf "%s" "$platform"
+}
+
+get_arch () {
+    local arch=""
+    case "$(uname -m)" in
+        x86_64|amd64) arch="amd64"; ;;
+        i686|i386|386) arch="386"; ;;
+        aarch64|arm64) arch="arm64"; ;;
+        *)
+            fail "Arch '${arch_check}' not supported!"
+            ;;
+    esac
+
+    printf "%s" "$arch"
 }
